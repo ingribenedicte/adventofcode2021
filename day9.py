cc47dc304ab7721ecdb4b10ca58841d28edd9e8a
Ingri Benedicte Rønning <31984367+ingribenedicte@users.noreply.github.com>
2022-01-03 11:34:27 +0100

    Added solution to Day 9 Part 2

--- day9.py
+++ day9.py
@@ -1,18 +1,16 @@
 from pathlib import Path
-import numpy as np
 
-path = Path('input\day9.txt')
+path = Path('heightmap.txt')
 problem = Path.read_text(path)
 height = problem.strip().split('\n')
 
+# Part 1:
 heightmap = []
 for h in height:
     hm = []
     for c in h:
         hm.append(int(c))
     heightmap.append(hm)
-heightmap = np.array(heightmap)
-print(heightmap)
 
 def get_neighbors(map, i, j):
     coordinates = []
@@ -43,70 +41,33 @@ def get_neighbors(map, i, j):
         coordinates.append((i, j - 1))
     return neighbors, coordinates
 
-def get_basin_neighbors(map, i, j):
-    coordinates = []
-    neighbors = []
-    n = len(map) - 1
-    m = len(map[0]) - 1
-    if i != 0 and i != n:
-        neighbors.append(map[i - 1][j])
-        neighbors.append(map[i + 1][j])
-        coordinates.append((i - 1, j))
-        coordinates.append((i + 1, j))
-    if i == 0:
-        neighbors.append(map[i + 1][j])
-        coordinates.append((i + 1, j))
-    if i == n:
-        neighbors.append(map[i - 1][j])
-        coordinates.append((i - 1, j))
-    if j != 0 and j != m:
-        neighbors.append(map[i][j - 1])
-        neighbors.append(map[i][j + 1])
-        coordinates.append((i, j - 1))
-        coordinates.append((i, j + 1))
-    if j == 0:
-        neighbors.append(map[i][j + 1])
-        coordinates.append((i, j + 1))
-    if j == m:
-        neighbors.append(map[i][j - 1])
-        coordinates.append((i, j - 1))
-
-    basin_neighbors = []
-    basin_coordinates = []
-    for k, bn in enumerate(neighbors):
-        if bn != 9:
-            basin_neighbors.append(bn)
-            basin_coordinates.append(coordinates[k])
-    return basin_neighbors, basin_coordinates
-
 low_points = []
 coordinates = []
 for i, row in enumerate(heightmap):
     row_length = len(row)
     for j, col in enumerate(row):
         neighbors, coor = get_neighbors(heightmap, i, j)
-
         if col < min(neighbors):
             low_points.append(col)
             coordinates.append((i, j))
 
-print(low_points, coordinates)
+print(sum(low_points) + len(low_points))
 
+# Part 2:
 basins = []
-for coordinate in coordinates:
-    i = coordinate[0]
-    j = coordinate[1]
-    basin = []
-    basin.append(heightmap[i][j])
-    coor_set = set()
+for i, coor in enumerate(coordinates):
+    basin_coor = [coor]
+    j = 0
+    unclosed = True
+    while unclosed:
+        neighbors, coor = get_neighbors(heightmap, basin_coor[j][0], basin_coor[j][1])
+        for n, c in zip(neighbors, coor):
+            if n < 9 and c not in basin_coor:
+                basin_coor.append(c)
+        if j == len(basin_coor) - 1:
+            break
+        j += 1
+    basins.append(len(basin_coor))
 
-    undiscovered = True
-    while undiscovered:
-        neighbors, coor = get_basin_neighbors(heightmap, i, j)
-        coor = set(coor)
-        if len(neighbors) == 0:
-            undiscovered = False
-        basin.extend(neighbors)
-        coor_set = coor_set.union(coor)
-
-    basins.append(len(basin))
+sizes = sorted(basins, reverse = True)[:3]
+print(sizes[0] * sizes[1] * sizes[2])
